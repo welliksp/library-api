89960072d28d1f59d0adb38a06c834e295f8c08c
Wellik Perroni <welliksp@gmail.com>
2026-07-26 16:16:31 -0300

    feat(repository): implementar LivroRepository com MongoRepository
    - Estende MongoRepository<LivroEntity, Long>
    - Adiciona busca por ISBN retornando Optional
    - Adiciona busca paginada por gênero
    - Adiciona verificação de existência por ISBN

--- src/main/java/br/com/wsp/library_api/repository/LivroRepository.java
+++ src/main/java/br/com/wsp/library_api/repository/LivroRepository.java
@@ -1,4 +1,20 @@
 package br.com.wsp.library_api.repository;
 
-public interface LivroRepository {
+import br.com.wsp.library_api.entity.LivroEntity;
+import br.com.wsp.library_api.entity.enums.Genero;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface LivroRepository extends MongoRepository<LivroEntity, Long> {
+
+
+    Optional<LivroEntity> findByIsbn(String isbn);
+
+    Page<LivroEntity> findByGenero(Genero genero, Pageable pageable);
+
+    boolean existsByIsbn(String isbn);
+
 }
